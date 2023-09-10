938e9b86aef615683976ac2637753a38e57c6f95
midhungopipk <midhungopipk@gmail.com>
2023-09-10 23:40:01 +0530

    express installed

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require('http');
-const routes = require('./routes');
+const http = require("http");
 
-const server = http.createServer(routes);
+const express = require("express");
+
+const app = express();
+app.use((req, res, next) => {
+  console.log("in middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in another middleware");
+  res.send("<h1>Hello from express js</h1>");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
