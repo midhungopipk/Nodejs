2669a294a3923f71ee5fb8d22c23b6b1bcb7638c
midhunsoftwareassociates <midhun@softwareassociates.in>
2023-10-02 22:35:17 +0530

    Cleaning up for using ejs

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-// const http = require('http');
-
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
-const exphbs = require('express-handlebars');
 const app = express();
 
-// app.engine(
-// 	'hbs',
-// 	exphbs({
-// 		layoutsDir: 'views/layouts/', //used to render layout for handlebar
-// 		defaultLayout: 'main-layout', //used to render layout for handlebar
-// 		extname: 'hbs', //used to render layout for handlebar
-// 	}),
-// );
-//switching to handlebars --> we can set hbs or handlebars just by ourself
-// app.set('view engine', 'hbs');//code if using handlebars
 app.set('view engine', 'ejs');
-// app.set('view engine', 'pug');
 app.set('views', 'views'); //This is the default anyway
 
 const adminData = require('./routes/admin');
@@ -30,7 +16,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-	// res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
 	res.status(404).render('404', { pageTitle: 'Error' });
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
-const rootDir = require('../util/path');
 const products = [];
 //This is actually /admin/add-products => GET
 router.get('/add-product', (req, res, next) => {
-	//res.sendFile(path.join(__dirname, '../','views', 'add-product.html'));
-	// res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-
-	// res.render('add-product', {
-	// 	pageTitle: 'Add Products',
-	// 	path: '/admin/add-product',
-	// });
 	res.render('add-product', {
 		pageTitle: 'Add Products',
 		path: '/admin/add-product',
